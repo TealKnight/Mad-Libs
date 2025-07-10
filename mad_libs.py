b1ee45729424e8391d384b7214cde82a3daede46
Jager Perez <jdhperez24@gmail.com>
2025-07-10 09:53:57 -0500

    Add files via upload

---
+++ mad_libs.py
@@ -0,0 +1,7 @@
+#First prompt the user to enter these questions
+user_adjective = input('Enter an adjective: ')
+user_past_tense_verb = input('Enter a past-tense verb: ')
+user_noun = input('Enter a noun: ')
+
+#Print out the users resonses into the paragraph below:
+print(f'In the 1800s, Ada Lovelace worked with Charles Babbage on a {user_adjective} engine. While the machine was never {user_past_tense_verb}, her notes include an algorithm intended for it.  For this, many consider her to be the first computer {user_noun}.')
